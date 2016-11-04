e5578f613da68c26a7455f40d77dcb1d5b42178b
liujie <liujie@broadtext.com.cn>
2016-11-04 08:45:30 +0800

    创建develop 分支 提交

--- src/SimpleFactoryPattern/Program1.java
+++ src/SimpleFactoryPattern/Program1.java
@@ -2,12 +2,7 @@ package SimpleFactoryPattern;
 
 public class Program1 {
 
-    /**
-     * <p>Description: TODO</p>
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
     }
 
--- src/SimpleFactoryPattern/Program2.java
+++ src/SimpleFactoryPattern/Program2.java
@@ -2,12 +2,7 @@ package SimpleFactoryPattern;
 
 public class Program2 {
 
-    /**
-     * <p>Description: TODO</p>
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
     }
 
