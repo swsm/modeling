8671e09f82f101f54c546c9e01a02a87f9c85447
liujie <liujie@broadtext.com.cn>
2016-11-04 08:29:11 +0800

    init commit

---
+++ src/SimpleFactoryPattern/Program1.java
@@ -0,0 +1,14 @@
+package SimpleFactoryPattern;
+
+public class Program1 {
+
+    /**
+     * <p>Description: TODO</p>
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/SimpleFactoryPattern/Program2.java
@@ -0,0 +1,14 @@
+package SimpleFactoryPattern;
+
+public class Program2 {
+
+    /**
+     * <p>Description: TODO</p>
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
