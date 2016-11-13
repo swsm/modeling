a6f838a4c38166e99dcc04cab67eff38c5ec467c
swsm <776502545@qq.com>
2016-11-13 21:09:54 +0800

    外观模式 代码提交

---
+++ src/FacadePattern/S2/Main.java
@@ -0,0 +1,11 @@
+package FacadePattern.S2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.methodA();
+        facade.methodB();
+    }
+
+}
---
+++ src/FacadePattern/S2/SubSystemOne.java
@@ -0,0 +1,7 @@
+package FacadePattern.S2;
+
+public class SubSystemOne {
+    public void methodOne() {
+        System.out.println("子系统方法一");
+    }
+}
